4aaacc0b93aa5127c3592a948b6c9c15a854474e
Cy Brown <cy.brown59@gmail.com>
2015-10-21 07:43:22 +0200

    Correction pour hoisting sur firefox

--- front/src/application.tsx
+++ front/src/application.tsx
@@ -12,41 +12,47 @@ function normalize(str: string): string {
     return (str || '').toLowerCase();
 }
 
-declare var fetch: any;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json');
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        bootstrapApp();
+    }
+};
+xhr.send();
 
-fetch('data.json')
-    .then((data: any) => data.json())
-    .then((songs: Song[]) => {
-        const artists: Dict<Artist> = songs.reduce((artists: Dict<Artist>, song: Song) => {
-            const normalizedArtist = normalize(song.artist) || 'Unknown artist';
-            const normalizedAlbum = normalize(song.album) || 'Unknown album';
-            artists[normalizedArtist] = artists[normalizedArtist] || {
-                name: song.artist || 'Unknown artist',
-                albums: newDict<Album>()
-            };
-            artists[normalizedArtist].albums[normalizedAlbum] = artists[normalizedArtist].albums[normalizedAlbum] || {
-                name: song.album || 'Unknown album',
-                songs: []
-            };
-            song.title = song.title || 'Unknown Title';
-            artists[normalizedArtist].albums[normalizedAlbum].songs.push(song);
-            return artists;
-        }, newDict<Artist>());
-        toArray(artists).forEach(artist => {
-            toArray(artist.albums).forEach(album => {
-                album.songs.sort((song1: Song, song2: Song) => {
-                    return song1.track - song2.track || song1.title.localeCompare(song2.title);
-                });
+function bootstrapApp() {
+    const songs = JSON.parse(xhr.responseText);
+    const artists: Dict<Artist> = songs.reduce((artists: Dict<Artist>, song: Song) => {
+        const normalizedArtist = normalize(song.artist) || 'Unknown artist';
+        const normalizedAlbum = normalize(song.album) || 'Unknown album';
+        artists[normalizedArtist] = artists[normalizedArtist] || {
+            name: song.artist || 'Unknown artist',
+            albums: newDict<Album>()
+        };
+        artists[normalizedArtist].albums[normalizedAlbum] = artists[normalizedArtist].albums[normalizedAlbum] || {
+            name: song.album || 'Unknown album',
+            songs: []
+        };
+        song.title = song.title || 'Unknown Title';
+        artists[normalizedArtist].albums[normalizedAlbum].songs.push(song);
+        return artists;
+    }, newDict<Artist>());
+    toArray(artists).forEach(artist => {
+        toArray(artist.albums).forEach(album => {
+            album.songs.sort((song1: Song, song2: Song) => {
+                return song1.track - song2.track || song1.title.localeCompare(song2.title);
             });
         });
-        const dispatcher = new Dispatcher(doRender);
-        const applicationStore = new ApplicationStore(dispatcher.when, songs);
-
-        function doRender() {
-            render(<MusicApplication store={applicationStore}
-                                    dispatcher={dispatcher} />,
-                document.getElementById("react-main"));
-        }
-        applicationStore.musicLibrary.artists = toArray(artists);
-        doRender();
     });
+    const dispatcher = new Dispatcher(doRender);
+    const applicationStore = new ApplicationStore(dispatcher.when, songs);
+
+    function doRender() {
+        render(<MusicApplication store={applicationStore}
+                                dispatcher={dispatcher} />,
+            document.getElementById("react-main"));
+    }
+    applicationStore.musicLibrary.artists = toArray(artists);
+    doRender();
+}
