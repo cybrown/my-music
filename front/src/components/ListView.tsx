013818cb8b7e832a2f346efd7c4cb57ab134a643
Cy Brown <cy.brown59@gmail.com>
2015-10-25 16:07:06 +0100

    Début fonctionnalité recherche

--- front/src/components/ListView.tsx
+++ front/src/components/ListView.tsx
@@ -11,7 +11,17 @@ interface IListViewProps<T> {
     footer?: string | JSX.Element;
 }
 
-export default class ListView<T> extends React.Component<IListViewProps<T>, {}> {
+interface IListViewState {
+    searchFieldVisible?: boolean;
+    searchValue?: string;
+}
+
+export default class ListView<T> extends React.Component<IListViewProps<T>, IListViewState> {
+
+    state: IListViewState = {
+        searchFieldVisible: false,
+        searchValue: null
+    };
 
     isActive(item: T): boolean {
         if (typeof this.props.activeItem === 'function') {
@@ -21,12 +31,56 @@ export default class ListView<T> extends React.Component<IListViewProps<T>, {}> 
         }
     }
 
+    showSearchField = () => this.setState({ searchFieldVisible: true });
+
+    hideSearchField = () => this.setState({ searchValue: null, searchFieldVisible: false });
+
+    onSearchChange = (event: React.FormEvent) => {
+        const value = (event as any).target.value;
+        this.state.searchValue = value;
+        setTimeout(() => 1);
+    };
+
+    onHideSearchField = (event: React.KeyboardEvent) => {
+        if (event.keyCode === 27) {
+            setTimeout(() => this.setState({ searchValue: null, searchFieldVisible: false }), 100);
+        }
+    }
+
+    createHeader = () => (
+        <div onClick={this.showSearchField}>
+            {this.state.searchFieldVisible ?
+                <div>
+                    <input type="text"
+                           className="form-control input-sm"
+                           autoFocus
+                           onKeyUp={this.onHideSearchField}
+                           onChange={this.onSearchChange} />
+                </div> :
+                <div>{this.props.header}</div>}
+        </div>
+    );
+
+    getItems = () => this.props.items.filter(item => {
+        if (this.state.searchValue == null) {
+            return true;
+        }
+        const regexp = new RegExp(this.state.searchValue, 'i');
+        if (typeof item === 'string') {
+            return (item as any as string).search(regexp) !== -1;
+        }
+        return Object.keys(item).filter(key => {
+            return typeof (item as any)[key] === 'string' && (item as any)[key].search(regexp) !== -1
+        }).length > 0;
+    });
+
     render() {
         return (
-            <Panel header={this.props.header} footer={this.props.footer || <span className="pull-right">{this.props.items.length + '  Elements'}</span>}>
+            <Panel header={this.createHeader()}
+                   footer={this.props.footer || <span className="pull-right">{this.props.items.length + '  Elements'}</span>}>
                 {this.props.items.length > 0 ?
                     <ListGroup>
-                        {this.props.items.map(item =>
+                        {this.getItems().map(item =>
                             <a className={'list-group-item ' + (this.isActive(item) ? 'active' : '')}
                                style={this.props.onClick ? {cursor: 'pointer'} : null}
                                key={this.props.keyFor(item)}
